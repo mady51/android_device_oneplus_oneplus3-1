03be41ab95107a5391d3410789f13135b0d769e5
SKULSHADY <anushekprasal@gmail.com>
2018-08-25 18:43:56 +0530

    Build Official

--- device.mk
+++ device.mk
@@ -316,3 +316,4 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp/mediacodec-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy \
     $(LOCAL_PATH)/seccomp/mediaextractor-seccomp.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediaextractor.policy
 
+export export HAVOC_BUILD_TYPE=Official
